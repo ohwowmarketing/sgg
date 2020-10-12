9cf919fb53e0a1aa31140ca3aae6cc16d72ddc0a
Jeremy Moseley <jeremymoseley@me.com>
2020-10-12 11:42:41 -0700

    Spread API
    Page also included with a sample table

---
+++ pages/[league]/spread.js
@@ -0,0 +1,89 @@
+import axios from 'axios'
+import Head from 'next/head'
+
+const score = (wins, losses, draws = 0) => {
+  return (draws > 0) ?  `${wins} - ${losses} - ${draws}` : `${wins} - ${losses}`;
+}
+
+export default function Spread({ league, spreads }) {
+
+  return (
+    <div className="container">
+      <Head>
+        <title>{league.toUpperCase()} Spread</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          {league.toUpperCase()} Spread
+        </h1>
+
+        <table id="ats-table" className="uk-table uk-table-divider" data-league="<?php echo $league; ?>">
+          <thead>
+            <tr>
+              <th className="team-label">Team</th>
+              <th>Overall</th>
+              {league !== 'nfl' && (
+                <>
+                <th>Home</th>
+                <th>Away</th>
+                </>
+              )}
+              <th><small>Last 10 Games</small> ATS Home</th>
+              <th><small>Last 10 Games</small> ATS Away</th>
+              <th><small>Last 10 Games</small> OV/UN Home</th>
+              <th><small>Last 10 Games</small> OV/UN Away</th>
+            </tr>
+          </thead>
+          <tbody>
+            {spreads.map((spread) => (
+              <tr key={spread.display}>
+                <td className="team-panel">
+                  <img src={spread.logo} style={{
+                    width: '36px',
+                    height: '24px',
+                    paddingRight: '12px',
+                    objectFit: 'contain'
+                  }} />
+                  {spread.display}
+                </td>
+                <td>{score(spread.wins, spread.losses)}</td>
+                {(league !== 'nfl') ? (
+                  <>
+                    <td></td>
+                    <td></td>
+                  </>
+                ) : null}
+                <td>
+                  {score(spread.homeSpreadWins, spread.homeSpreadLosses, spread.homeSpreadPushes)}
+                </td>
+                <td>
+                  {score(spread.awaySpreadWins, spread.awaySpreadLosses, spread.awaySpreadPushes)}
+                </td>
+                <td>
+                  {score(spread.homeOvers, spread.homeUnders, spread.homeOverUnderPushes)}
+                </td>
+                <td>
+                  {score(spread.awayOvers, spread.awayUnders, spread.awayOverUnderPushes)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+
+      <footer>
+        <p>SGG</p>
+      </footer>
+    </div>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const { league } = params;
+  const { data } = await axios(`http://localhost:3000/api/${league}/spread`);
+  return {
+    props: { league, spreads: data },
+  }
+}
---
+++ pages/api/[league]/spread.js
@@ -0,0 +1,109 @@
+import axios from 'axios';
+import { connectToDatabase } from '../../../util/mongodb';
+require('events').EventEmitter.defaultMaxListeners = 40;
+
+const getTeams = async (league) => {
+  const { data } = await axios(`${process.env.API_URL}/api/sdio/${league}/teams`);
+  return data;
+}
+
+const getStandings = async (league) => {
+  const { data: season } = await axios(`${process.env.API_URL}/api/sdio/${league}/season`);
+  const { data: standings } = await axios(`${process.env.API_URL}/api/sdio/${league}/standings/${season.current}`);
+  return standings;
+}
+
+const getTrends = async (league, abbr) => {
+  const { data } = await axios(`${process.env.API_URL}/api/sdio/${league}/trends/${abbr}`);
+  return data;
+}
+
+const getTeam = (id, teams) => teams.find((team) => team.sdio === id)
+
+const getRow = async (league, standing, teams) => {
+  const team = await getTeam(standing.sdio, teams);
+  const trends = await getTrends(league, team.abbr);
+  return {
+    logo: team.logo,
+    display: team.display,
+    wins: standing.wins,
+    losses: standing.losses,
+    homeSpreadWins: trends.home.spreadWins,
+    homeSpreadLosses: trends.home.spreadLosses,
+    homeSpreadPushes: trends.home.spreadPushes,
+    awaySpreadWins: trends.away.spreadWins,
+    awaySpreadLosses: trends.away.spreadLosses,
+    awaySpreadPushes: trends.away.spreadPushes,
+    homeOvers: trends.home.overs,
+    homeUnders: trends.home.unders,
+    homeOverUnderPushes: trends.home.overUnderPushes,
+    awayOvers: trends.away.overs,
+    awayUnders: trends.away.unders,
+    awayOverUnderPushes: trends.away.overUnderPushes
+  }
+}
+
+const getSpreads = async (league) => {
+  console.time(`${league.toUpperCase()} Spread`);
+
+  const { db } = await connectToDatabase();
+  const cacheTime = process.env.BRIEF_CACHE_MINS * 60 * 1000;
+  const cache = await db
+    .collection("spread")
+    .findOne({
+      _id: `${league}`,
+      updated_at: {
+        $gte: new Date(new Date().getTime() - cacheTime).toISOString()
+      }
+    })
+
+  if (cache) {
+    console.timeEnd(`${league.toUpperCase()} Spread`);
+    return cache.data;
+  }
+
+  console.log(`${league.toUpperCase()} Spread: [cache miss...]`);
+
+  const teams = await getTeams(league);
+  const standings = await getStandings(league);
+  const spreads = await Promise.all(standings.map(async (standing) => getRow(league, standing, teams)));
+  
+  spreads.sort((a, b) => {
+    if (a.wins === b.wins) {
+      if (a.losses === b.losses) {
+        return a.display.localeCompare(b.display);
+      }
+      return a.losses < b.losses ? -1 : 1;
+    }
+    return a.wins > b.wins ? -1 : 1;
+  });
+
+  await db
+  .collection("spreads")
+  .updateOne(
+    { _id: league },
+    { $set: { data: spreads, updated_at: new Date().toISOString() } },
+    { upsert: true }
+  )
+
+  console.timeEnd(`${league.toUpperCase()} Spread`);
+
+  return spreads;
+}
+
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  const { query: { league }} = req;
+  const spreads = await getSpreads(league);
+  res.status(200).json(spreads);
+};
