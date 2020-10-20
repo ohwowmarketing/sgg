942b9776d7c76ea2275cb8be742eb968f609f2a3
Jeremy Moseley <jeremymoseley@me.com>
2020-10-20 10:10:01 -0700

    Grab cache if available

--- pages/api/[league]/future/[market].js
+++ pages/api/[league]/future/[market].js
@@ -282,8 +282,8 @@ const getRows = async (league, ids, futures, meta, sportsbooks, teams) => {
 }
 
 const getFuturesByMarket = async (league, market) => {
-  // const cache = await isCached(league, market);
-  // if (cache) { return cache; }
+  const cache = await isCached(league, market);
+  if (cache) { return cache; }
 
   const allFutures = await getFutures(league);
   const futures = allFutures.filter((future) => future.bet === parseInt(market));
